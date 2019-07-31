a5763fbd894b00e429943ec63324c5c00b881874
pradeep G <pradeep@vitallabsinc.com>
2019-08-01 00:53:44 +0530

    #ignoring /

--- src/main/java/com/fyle/interview/controller/Bank.java
+++ src/main/java/com/fyle/interview/controller/Bank.java
@@ -18,6 +18,11 @@ public class Bank {
     @Autowired
     private BankService bankService;
 
+    @RequestMapping("/")
+    public String hello() {
+        return "Hello there, please use the curl scripts with JWT token to try out the APIs";
+    }
+
     @RequestMapping("/api/bank/{name}/{city}")
     public List<BankDetailsModel> bank(@PathVariable String name,
                                        @PathVariable String city,
